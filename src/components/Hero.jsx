f6785975d8b91d0c8f0ed483f135fbd06cdd3db6
khalid-dev24 <khalidmisbaudeen04@gmail.com>
2025-04-29 13:57:39 +0100

    Enhance UI and functionality across components
    - Updated Footer component to include Instagram icon with link.
    - Modified Hero component layout for better button alignment and added a column for buttons.
    - Improved Navbar by adding Instagram icon and link, and removed unused Facebook icon.
    - Revamped Testimonials component to include a carousel effect for displaying testimonials with navigation arrows.
    - Refined CSS styles for testimonials, hero section, and social media icons for better responsiveness and aesthetics.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,12 @@ function Hero() {
       <div className="hero-text">
         <h1 style={{ fontFamily: 'Bebas Neue, sans-serif' }}>Optimize Your Business <br /> with a Trusted <br /> <span>ERP & CRM</span></h1>
         <p>A powerful ERP & CRM designed to streamline <br />operations, enhance collaboration, and drive <br />business growth.</p>
-        <button className="hero-btn">Try For Free <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.371094 9.3457V6.99219L7.19727 4.81445L0.371094 2.49023V0.351562L9.63867 3.92578V5.91797L0.371094 9.3457Z" fill="#2C2C31"/>
-          </svg>
-        </button> <span><a href="#contactus">Talk to an Expert</a></span>
+        <div className="column">
+          <button className="hero-btn">Try For Free <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.371094 9.3457V6.99219L7.19727 4.81445L0.371094 2.49023V0.351562L9.63867 3.92578V5.91797L0.371094 9.3457Z" fill="#2C2C31"/>
+            </svg>
+          </button> <span><a href="#contactus">Talk to an Expert</a></span>
+        </div>
        
       </div>
       <div className="hero-image">
